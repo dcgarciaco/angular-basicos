0ca17e1ad70cbafdd017bce49f95c8a0d7d09662
Diana Garcia <dcgarciaco@unal.edu.co>
2023-01-23 09:26:41 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+/** Tarea:
+ *  Crear un módulo llamado ContadorModule
+ * Realizar las declaraciones y exportaciones
+ */
+import {ContadorModule} from './contador/contador.module'
+
+import { HeroesModule } from './heroes/heroes.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
